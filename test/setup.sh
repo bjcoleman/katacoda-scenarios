9276043893536cde822f56ca1e2ad4ac55d9d82f
Ben Coleman <colemanb@moravian.edu>
2019-12-20 14:14:03 -0500

    change order of commands (debugging)

--- test/setup.sh
+++ test/setup.sh
@@ -4,9 +4,9 @@ echo 'keeper ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 git clone https://github.com/git-keeper/git-keeper.git
 pip3 install git-keeper/git-keeper-core
 pip3 install git-keeper/git-keeper-server
+cp git-keeper/tests/acceptance/files/valid_server.cfg ~keeper/server.cfg
+chown keeper ~keeper/server.cfg
 apt-get install screen -y
 mkdir /email
 screen -S gkmail -d -m python3 git-keeper/tests/acceptance/gkserver_base/mysmtpd.py 25 /email
-cp git-keeper/tests/acceptance/files/valid_server.cfg ~keeper/server.cfg
-chown keeper ~keeper/server.cfg
 runuser -l keeper -c "screen -S gkserver -d -m gkeepd"
