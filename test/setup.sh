ffd16d5d5c048833f7950b3517f4dcce3ac69c13
Ben Coleman <colemanb@moravian.edu>
2019-12-20 14:19:19 -0500

    bug fix

--- test/setup.sh
+++ test/setup.sh
@@ -6,7 +6,7 @@ pip3 install git-keeper/git-keeper-core
 pip3 install git-keeper/git-keeper-server
 cp git-keeper/tests/acceptance/files/valid_server.cfg ~keeper/server.cfg
 chown keeper ~keeper/server.cfg
-apt-get install screen -y
+apt-get install screen -y &&
 mkdir /email
 screen -S gkmail -d -m python3 git-keeper/tests/acceptance/gkserver_base/mysmtpd.py 25 /email
 runuser -l keeper -c "screen -S gkserver -d -m gkeepd"
