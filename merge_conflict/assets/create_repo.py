e31cf447e891f1f607719d7db0c6b90329cb3837
Ben Coleman <colemanb@moravian.edu>
2022-03-16 13:28:07 -0400

    create a repo and print results

--- merge_conflict/assets/create_repo.py
+++ merge_conflict/assets/create_repo.py
@@ -1,2 +1,16 @@
+import requests
+import json
 
-print('hello world')
+
+print('Creating a repo for us to use in the scenario...')
+url = 'https://lfvvlgcpn8.execute-api.us-east-1.amazonaws.com/prod/create_repo'
+result = requests.get(url)
+
+
+print('\n\nSuccess!!\n\n')
+
+body = result.json()['body']
+data = json.loads(body)
+
+print('Your repo is named {}.'.format(data['repo_name']))
+print('You can access your repo at {}'.format(data['repo_url']))
